6c7dd15fb0dc6f5236bcd47eb0a27f3fd16bea31
Ashwin Raghav <ashwinraghav@google.com>
2024-03-31 12:51:56 -0700

    Update idx-template.nix

--- nextjs/idx-template.nix
+++ nextjs/idx-template.nix
@@ -9,10 +9,16 @@
 
     bootstrap = ''
 			mkdir "$out"
+			mkdir -p /home/user/.pnpm-store
+			cd /home/user/.pnpm-store
+			wget -O p.tar.gz "https://firebasestorage.googleapis.com/v0/b/devfest-c4c8f.appspot.com/o/p.tar.gz?alt=media&token=5f99226c-8b17-4ffb-b5c3-fcf15ccbcd3a"
+			tar --no-overwrite-dir -mxf p.tar.gz
+			rm p.tar.gz
+			pnpm config set store-dir /home/user/.pnpm-store
 			pnpm create next-app "$out" \
 					--import-alias=${importAlias} \
 					--${language} \
-					--use-npm \
+					--use-pnpm \
 					${if eslint then "--eslint" else "--no-eslint" } \
 					${if srcDir then "--src-dir" else "--no-src-dir" } \
 					${if app then "--app" else "--no-app" } \

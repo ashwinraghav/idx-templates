79fc44d65ce859ca2d506c111f60e0df8a4e82eb
Ashwin Raghav <ashwinraghav@google.com>
2024-03-31 12:58:35 -0700

    Update idx-template.nix

--- nextjs/idx-template.nix
+++ nextjs/idx-template.nix
@@ -11,7 +11,8 @@
 			mkdir "$out"
 			mkdir -p /home/user/.pnpm-store
 			cd /home/user/.pnpm-store
-			wget -O p.tar.gz "https://firebasestorage.googleapis.com/v0/b/devfest-c4c8f.appspot.com/o/p.tar.gz?alt=media&token=5f99226c-8b17-4ffb-b5c3-fcf15ccbcd3a"
+			curl -0 "https://firebasestorage.googleapis.com/v0/b/devfest-c4c8f.appspot.com/o/p.tar.gz?alt=media&token=5f99226c-8b17-4ffb-b5c3-fcf15ccbcd3a" -o p.tar.gz
+			# wget -O p.tar.gz "https://firebasestorage.googleapis.com/v0/b/devfest-c4c8f.appspot.com/o/p.tar.gz?alt=media&token=5f99226c-8b17-4ffb-b5c3-fcf15ccbcd3a"
 			tar --no-overwrite-dir -mxf p.tar.gz
 			rm p.tar.gz
 			pnpm config set store-dir /home/user/.pnpm-store

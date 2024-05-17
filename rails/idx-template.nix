1d2dfa0dd56ad35e90f1fded75913c1300cd64c8
Ashwin Raghav <ashwinraghav@google.com>
2024-05-17 12:04:35 -0700

    Create idx-template.nix

---
+++ rails/idx-template.nix
@@ -0,0 +1,13 @@
+{ pkgs, version ? "latest", importAlias ? "@/*",  language ? "ts", packageManager ? "pnpm", srcDir ? false, eslint ? false, app ? false, tailwind ? false, ... }: {
+	
+    packages = [
+			pkgs.ruby
+      pkgs.nodejs
+      pkgs.yarn
+      pkgs.sqlite
+    ];
+
+    bootstrap = ''
+       gem install rails
+    '';
+}
